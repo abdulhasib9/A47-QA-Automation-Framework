16faa763f41194e42b3dbf371ce0954e6f4cfd41
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2023-11-06 12:57:41 -0800

    added homwork19 class and code example, also added homework-19 xml config file

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,45 @@
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Homework19 {
+    @Test
+    @Parameters("baseUrl")
+    public void deletePlaylist(String baseUrl) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get(baseUrl)
+
+        //login
+        driver.findElement(By.xpath("//input[@placeholder='Email Address']")).sendKeys("username.testpro.io");
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("password");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+
+        //deleting playlist aa
+        driver.findElement(By.xpath("//button[@title='Delete this playlist']")).click();
+        //confirming the deletion by pressing the ok button in model popup
+        driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/nav[1]/button[2]")).click();
+
+
+
+        //checking deletion confirmation
+        String playlistName = driver.findElement(By.xpath("//*[@id=\"playlistWrapper\"]/header/div[2]/h1/text()").getText())
+        WebElement successToaster =driver.findElement(By.xpath("///html[1]/body[1]/div[2]/div[1]"));
+        string successToasterActiveClass = pauseButton.getAttribute("class");
+        string desiredClass = "success show";
+
+
+        if (successToasterActiveClass.contains(desiredClass)) {
+            print("Deleted playlist "+playlistName)
+        } else {
+            print("Error occured !!!")
+        }
+
+        driver.quit();
+
+    }
+}
